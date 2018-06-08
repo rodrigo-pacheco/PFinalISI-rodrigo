dd524afc09bd2678953f907735dd3a7a3a7f696f
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-06-08 09:55:10 +0200

    Pruebo si interpreta HTML introduciendo un titulo

--- src/main/java/urjc/isi/PFinalISI/Main.java
+++ src/main/java/urjc/isi/PFinalISI/Main.java
@@ -10,7 +10,7 @@ import java.net.URISyntaxException;
 public class Main {
   
     public static String doWork(Request request, Response response) throws ClassNotFoundException, URISyntaxException {
-	String result = new String("Hello World");
+	String result = new String("<h1>Hello World</h1>");
 
 	return result;
     }

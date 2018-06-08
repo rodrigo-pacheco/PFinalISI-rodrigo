fd4ad65ac484b2b63018ab497cd1a31538225e95
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-06-08 11:42:46 +0200

    Completado el recurso / y los recursos GET de /actor y /film

--- src/main/java/urjc/isi/PFinalISI/Main.java
+++ src/main/java/urjc/isi/PFinalISI/Main.java
@@ -8,12 +8,33 @@ import spark.Response;
 import java.net.URISyntaxException;
 
 public class Main {
+	
+    public static String doSearchFilm(Request request, Response response) throws ClassNotFoundException, URISyntaxException {
+		String result = new String("<h1>Search to know more about a film</h1></br>" + 
+								   "<p><form action=\"https://pfinal-isi-rodrigo.herokuapp.com/film\" method=\"post\">" +
+								   "Actor: <input type=\"text\" name=\"film\"><br>" + 
+								   "<input type=\"submit\" value=\"Search\"></form></p>");
+		return result;
+    }
+	
+    public static String doSearchActor(Request request, Response response) throws ClassNotFoundException, URISyntaxException {
+		String result = new String("<h1>Search to know more about an actor</h1></br>" + 
+								   "<p><form action=\"https://pfinal-isi-rodrigo.herokuapp.com/actor\" method=\"post\">" +
+								   "Actor: <input type=\"text\" name=\"actor\"><br>" + 
+								   "<input type=\"submit\" value=\"Search\"></form></p>");
+		return result;
+    }
   
     public static String doMainInfo(Request request, Response response) throws ClassNotFoundException, URISyntaxException {
-		String result = new String("<h1>Welcome to All About Films</h1></br>" +
-				 				   "<p>This are our options:</br>" + 
-								   "To load the Data Base click this" +
-				 				   "<a href=\"https://pfinal-isi-rodrigo.herokuapp.com/load\"> link</a></br></p>");
+		String result = new String("<h1>'' All About Films ''</h1></br>" +
+				 				   "<p>What can you do?:</br></br>" + 
+								   "To load the Data Base click this " +
+				 				   "<a href=\"https://pfinal-isi-rodrigo.herokuapp.com/load\">link</a></br>" + 
+								   "To search for an actor's films click this " + 
+								   "<a href=\"https://pfinal-isi-rodrigo.herokuapp.com/actor\">link</a></br>" + 
+								   "To search for an the actors casting in a film click this" + 
+								   "<a href=\"https://pfinal-isi-rodrigo.herokuapp.com/film\">link</a></br>" + 
+				 				   "</p>");
 	
 		return result;
     }
@@ -24,8 +45,8 @@ public class Main {
         // spark server
         get("/", 	 Main::doMainInfo);
 //        get("/load",  Main:: doLoadDDBB);
-//        get("/actor", Main:: doSearchActor);
-//        get("/film", Main:: doSearchFilm);
+        get("/actor", Main:: doSearchActor);
+        get("/film", Main:: doSearchFilm);
 
     }
 

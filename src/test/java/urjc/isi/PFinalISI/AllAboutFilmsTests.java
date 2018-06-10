57b244ba88c6d0d90bf22dac54690c0aea074480
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-06-10 11:02:48 +0200

    Ahora funciona el test pero no da lo esperado

--- src/test/java/urjc/isi/PFinalISI/AllAboutFilmsTests.java
+++ src/test/java/urjc/isi/PFinalISI/AllAboutFilmsTests.java
@@ -10,19 +10,20 @@ import static org.mockito.Mockito.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.xml.ws.Response;
-
-import org.eclipse.jetty.http.MetaData.Request;
 import org.junit.Test;
+
+import spark.Request;
+import spark.Response;
 
 public class AllAboutFilmsTests {
 	
-	// Test para doMainInfo
+	// Test para doLoadDDBB
 	@Test()
-	public void doMainInfo1() {
+	public void doMainInfo1() throws SQLException {
 		Request request = null;
 		Response response = null;
 		Main.doLoadDDBB(request, response);
